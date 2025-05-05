dfe784fddf6a731d9cdeaa4b269015b93d777a30
Leonardo López <lopezvelasquezleonardodavid@gmail.com>
2025-05-05 17:04:52 -0600

    proyecto

--- Lopez_Leonardo_ProyectoTienda/src/lopez_leonardo_proyectotienda/Lopez_Leonardo_ProyectoTienda.java
+++ Lopez_Leonardo_ProyectoTienda/src/lopez_leonardo_proyectotienda/Lopez_Leonardo_ProyectoTienda.java
@@ -45,7 +45,7 @@ public class Lopez_Leonardo_ProyectoTienda {
                     System.out.print("Ingrese monto en efectivo: ");
                     double monto = leer.nextDouble();
                     efectivoCaja += monto;
-                    System.out.println("\n===========**¡Monto agregado con éxito en caja!**===========");
+                    System.out.println("\n===========**¡Monto agregado con éxito en caja!**=========");
                     System.out.println("\n==========================================================");
 
                     break;
@@ -82,7 +82,7 @@ public class Lopez_Leonardo_ProyectoTienda {
 
                 // SECCIÓN DE COMPRAS
                 case 3:
-                    System.out.println("========================** COMPRAS **========================");
+                    System.out.println("========================** COMPRAS **======================");
                     System.out.println("==========================================================\n");
                     System.out.print("Ingrese tipo de Proveedor: ");
                     tipoProveedor = leer.next();
@@ -94,8 +94,9 @@ public class Lopez_Leonardo_ProyectoTienda {
                     if (tipoProveedor.equals("A")) {
                         if (tipoProducto == 2 || tipoProducto == 3) {
                             System.out.println("======** NO PUEDE PROVEER DICHO PRODUCTO **======\n");
+                            break;
                         }
-                        break;
+                        
                     } else if (tipoProveedor.equals("B")) {
                         if (tipoProducto == 1 || tipoProducto == 4) {
                             System.out.println("======** NO PUEDE PROVEER DICHO PRODUCTO **======\n");
@@ -113,13 +114,30 @@ public class Lopez_Leonardo_ProyectoTienda {
                     cantidadKg = leer.nextDouble();
                     switch (tipoProducto) {
                         case 1:
-                            
+
                             totalCompraProveedor = cantidadKg * 25;
                             System.out.println("Precio por kilogramo: " + 25 + " Lps.");
                             System.out.println("Precio total: " + totalCompraProveedor + " Lps.");
-                            azucarKg += cantidadKg;
-                            System.out.println("\n==========================================================");
+                            System.out.print("¿Desea realizar la compra?: ");
+                            String respuestaSiNo = leer.next().toLowerCase();
+
+                            if (respuestaSiNo.equals("si")) {
+                                if (efectivoCaja >= totalCompraProveedor) {
+                                    azucarKg += cantidadKg;
+                                    efectivoCaja -= totalCompraProveedor;
+                                } else {
+                                    System.out.println("No se puede pagar compra...");
+                                    System.out.println("\n==========================================================");
+                                    break;
+                                }
+                            } else {
+                                System.out.println("Finalizando proceso de compra...");
+                                System.out.println("\n==========================================================");
+                                break;
+                            }
+
                             break;
+
                         case 2:
                             if (tipoProveedor.equals("B")) {
                                 totalCompraProveedor = cantidadKg * 20;
@@ -130,7 +148,7 @@ public class Lopez_Leonardo_ProyectoTienda {
                             }
                             System.out.println("Precio total: " + totalCompraProveedor + " Lps.");
                             break;
-                        case 3: 
+                        case 3:
                             totalCompraProveedor = cantidadKg * 30;
                             System.out.println("Precio por kilogramo: " + 30 + " Lps.");
                             System.out.println("Precio total: " + totalCompraProveedor + " Lps.");
